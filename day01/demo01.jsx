99f44b11dabd536d9c9ec47224927ec8bc1e706e
sheldon <823231684@qq.com>
2016-08-23 14:43:00 +0800

    add  props   states

--- day01/demo01.jsx
+++ day01/demo01.jsx
@@ -1,39 +1,88 @@
 var Hello = React.createClass(
     {
         render: function () {
-
             return (
+
                 <li>
                     Hello {
                         this.props.name
                     }
-
                 </li>
-            );
+            )
+
         }
     }
 );
+var World = React.createClass({
+    getDefaultProps() {
+        return {
+            name: 'bbbbbbbbbbbbbbbbbbbb'
+        }
+
+    },
+    render() {
+        return (
+            <div>{this.props.name }</div>
+        )
+    }
+});
+
+var Checkbox = React.createClass({
+    handleChange() {
+        this.setState({
+            check: !this.state.check
+        })
+    },
+    getInitialState() {
+        return {
+            check: true
+        }
+    },
+    render() {
+        /* state对象管理状态 */
+        /* props对象管理 */
+        console.log('render  again')
+        return (
+            <div>
+                {/*驼峰命名*/}
+                <input onChange={this.handleChange} type='checkbox' checked={this.state.check} />
+            </div>
+        )
+
+    }
+});
+
+
+//定义无状态组件
+function StatelessComponent(props) {
+    return <div> Hello {props.name} </div>
+}
+
+
 
 var arr = ['a', 'b', 'c'];
-
 var ListView = React.createClass({
     render() {
         /*注释的写法 */
-
         var s = arr.map(function (el, index) {
             return (
                 <Hello key={index}  name={el}  />
-            )
+            );
         });
         return (
-            <ul>
-                {s}
-            </ul>
+            <div>
+                <Checkbox/>
+                <div>{this.props.name}</div>
+                <ul>
+                    { s }
+                </ul >
+                <World/>
+                <StatelessComponent  name='yaowenzhu'/>
+            </div>
         );
     }
-})
+});
 ReactDOM.render(
-
     <ListView  name='aaaaaa'/>, document.getElementById('example')
 );
 
--- readme.md
+++ readme.md
@@ -1,7 +1,44 @@
-#学习react ，类比之前学过的一点vue.js
-##day01
+# 学习react ，类比之前学过的一点vue.js
+
+## day01
 
     1. js的迭代函数map的用法(注意他有返回值);
-    2. 两个重要的API, React.createClass({  render (){}})方法，     ReactDOM.render(component,  挂载点)
 
-##day02
+    2. 两个重要的API
+
+    定义有状态的组件
+     React.createClass(  {render (){}})方法
+
+     定义无状态的组件(普通函数)
+
+     function StatelessComponent(props) {
+             return <div> Hello {props.name} </div>
+      }
+
+
+     ReactDOM.render(component,  挂载点)
+
+     3.  组件和状态(共同决定组建的render结果)
+
+     区别:
+     
+     组件状态维护在组件内部，属性是有外部传入组件,由外部控制
+
+     状态API:
+
+     getInitialState(){
+
+         return  变量  
+         
+     }
+
+     属性API:
+     
+    getDefaultProps(){
+
+    }
+
+
+
+
+## day02
